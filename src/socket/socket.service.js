cfc8196b070d5b8c674b07aa512ae377b0560c84
Aadarsh Dangi <aadarsht0001@gmail.com>
2025-08-23 18:38:49 +0530

    socket middleware set

--- src/socket/socket.service.js
+++ src/socket/socket.service.js
@@ -1,5 +1,8 @@
 import { Server } from "socket.io";
 import {generateContent} from "../services/ai-service.js";
+import cookie from "cookie";
+import jwt from "jsonwebtoken";
+import userModel from "../models/user.model.js";
 
 function setupSocketServer(server) {
   const io = new Server(server, {
@@ -7,6 +10,26 @@ function setupSocketServer(server) {
       origin: "*",
       methods: ["GET", "POST"]
     }
+  });
+
+ io.use(  async  (socket, next) => {
+    const cookies = cookie.parse(socket.handshake.headers?.cookie || '');
+    if(!cookies.token) {
+      return next(new Error("Authentication error"));
+    }
+    try {
+      
+      const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+      const user = await userModel.findById(decoded.id);
+      socket.user= user
+      next();
+
+    } catch (error) {
+      return next(new Error("Authentication error"));
+      
+    }
+    
+    next();
   });
 
   io.on("connection", (socket) => {
